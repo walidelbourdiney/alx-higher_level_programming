49156bb628ce5c9b909532003a7731cdd4394310
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-03-13 03:49:40 -0700

    task 09

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+i#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number < 0:
+        last_digit = 10 - (number % 10)
+        print(last_digit, end='')
+    else:
+        last_digit = number % 10
+        print(last_digit, end='')
+    return last_digit
