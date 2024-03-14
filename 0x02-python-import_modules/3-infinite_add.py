cb4c227d007eadb74c828ca9d7d3699895c9c296
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-03-14 16:10:57 -0700

    task 3/

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+        """Prints the result of the addition of all arguments"""
+    import sys
 
-import sys
-
-
-def add_argvs():
-    argvs = sys.argv
-    sum_argvs = 0
-    for i in range(1, len(argvs)):
-        sum_argvs += int(argvs[i])
-
-    print(sum_argvs)
-
-
-if __name__ == '__main__':
-    add_argvs()
+    all = 0
+    for i in range(len(sys.argv) - 1):
+        all += int(sys.argv[i + 1])
+    print("{}".format(all))
