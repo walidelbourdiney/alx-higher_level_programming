b3699bdbbec78c852a40096d6eb07dd9e0a49ceb
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-03-14 16:05:40 -0700

    task 2//

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-        """Prints the result of the addition of all arguments"""
-    import sys
+import sys
 
-    count = len(sys.argv) - 1
-
-                    # Convert arguments to integers and calculate the sum
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+if __name__ == '__main__':
+    argv = sys.argv
+    if len(argv) == 1:
+        print('{} arguments.'.format(len(argv) - 1))
+    elif len(argv) == 2:
+        print('{} argument:'.format(len(argv) - 1))
+        print('{}: {}'.format(len(argv) - 1, argv[len(argv) - 1]))
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print('{} arguments:'.format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
