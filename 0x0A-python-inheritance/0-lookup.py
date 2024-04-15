33f2a8b7bcda9f22ac637ba1ecbb93238746e392
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-15 16:32:08 -0700

    task 0/

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,7 +3,7 @@
 
 
 def lookup(obj):
-        """Function that returns the list of available
-            attributes and methods of an object.
-                """
-                    return dir(obj)
+    """Function that returns the list of available
+        attributes and methods of an object.
+    """
+    return dir(obj)
