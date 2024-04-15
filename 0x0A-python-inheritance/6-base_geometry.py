2ae78505954f1024a6adb448ac3fd8be8b45d42c
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-15 16:44:26 -0700

    task 06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module doc string"""
+
+
+class BaseGeometry:
+    """BaseGeometry class doc string"""
+
+    def area(self):
+        """Function doc string"""
+        raise Exception("area() is not implemented")
