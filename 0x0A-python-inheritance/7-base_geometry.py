326f1cb2ef90ac48fdb51623e15494d66013fb39
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-15 16:49:27 -0700

    task 07/

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """Function doc string"""
-            raise Exception("area() is not implemented")
+         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Function doc string"""
