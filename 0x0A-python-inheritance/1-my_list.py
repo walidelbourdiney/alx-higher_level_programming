f7fc56e9b82a1289d375d2b6f6a9272fc8c5027c
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-15 16:36:58 -0700

    task 01

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module doc string"""
+
+
+class MyList(list):
+    """MyList doc string"""
+
+    def print_sorted(self):
+        """print_sorted doc string"""
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)i
