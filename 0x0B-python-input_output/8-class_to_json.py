a22f27fd241dee7615eccdeabdf6ac4ac9782ad6
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-16 04:42:56 -0700

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module doc string"""
+
+
+def class_to_json(obj):
+    """Function that returns the dictionary
+    description with simple data structure.
+    """
+    return obj.__dict__
