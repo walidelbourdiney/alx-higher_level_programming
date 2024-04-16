0f644d79115d96bdee3a5225b43520fd76d4bc34
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-16 04:40:35 -0700

    task 6

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module doc string"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+        new_args = load_from_json_file('add_item.json')
+except Exception:
+    new_args = []
+
+for arg in range(1, len(sys.argv)):
+    new_args.append(sys.argv[arg])
+
+save_to_json_file(new_args, 'add_item.json')
