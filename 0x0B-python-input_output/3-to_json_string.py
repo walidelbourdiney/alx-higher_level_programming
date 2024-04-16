0fee21d14bdd5d8810b9b32c07017302ddd626aa
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-04-16 04:36:50 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module doc string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object (string)."""
+    return json.dumps(my_obj)
