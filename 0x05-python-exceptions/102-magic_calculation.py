59293b54ee322ae07647282a73e0f41709e558b2
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-05-06 17:07:58 -0700

    task 01-103

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    ans = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            ans += a ** b / i
+        except Exception:
+            ans = b + a
+            break
+    return ans
---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """TypeError exception."""
+    raise TypeError
